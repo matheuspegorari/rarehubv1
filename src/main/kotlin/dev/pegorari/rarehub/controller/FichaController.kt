de85e5b7820968c125fe344669232bf4af946458
Matheus Pegorari <pegorari42@gmail.com>
2024-10-23 10:57:42 -0300

    fix: changed to POST request

--- src/main/kotlin/dev/pegorari/rarehub/controller/FichaController.kt
+++ src/main/kotlin/dev/pegorari/rarehub/controller/FichaController.kt
@@ -2,17 +2,14 @@ package dev.pegorari.rarehub.controller
 
 import dev.pegorari.rarehub.dto.ProductRequest
 import dev.pegorari.rarehub.service.FichaService
-import org.springframework.web.bind.annotation.GetMapping
-import org.springframework.web.bind.annotation.RequestBody
-import org.springframework.web.bind.annotation.RequestMapping
-import org.springframework.web.bind.annotation.RestController
+import org.springframework.web.bind.annotation.*
 import reactor.core.publisher.Mono
 
 @RestController
 @RequestMapping("/fci")
 class FichaController(private val service: FichaService) {
 
-    @GetMapping
+    @PostMapping
     fun teste(@RequestBody products : List<ProductRequest>): Mono<String> {
         return service.getBearerToken()
             .flatMap { login ->
