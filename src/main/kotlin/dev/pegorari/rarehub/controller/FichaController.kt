1410ecd3ccb55e7ef2ba99623cd66797dd28132e
Matheus Pegorari <pegorari42@gmail.com>
2024-10-23 11:14:50 -0300

    exception: exception handling

--- src/main/kotlin/dev/pegorari/rarehub/controller/FichaController.kt
+++ src/main/kotlin/dev/pegorari/rarehub/controller/FichaController.kt
@@ -2,6 +2,8 @@ package dev.pegorari.rarehub.controller
 
 import dev.pegorari.rarehub.dto.ProductRequest
 import dev.pegorari.rarehub.service.FichaService
+import org.springframework.http.HttpStatus
+import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.*
 import reactor.core.publisher.Mono
 
@@ -10,16 +12,21 @@ import reactor.core.publisher.Mono
 class FichaController(private val service: FichaService) {
 
     @PostMapping
-    fun getFCIFromProducts(@RequestBody products : List<ProductRequest>): Mono<String> {
+    fun getFCIFromProducts(@RequestBody products: List<ProductRequest>): Mono<ResponseEntity<String>> {
         return service.getBearerToken()
             .flatMap { login ->
                 val bearerToken = login.bearerToken
                 service.getFCI(bearerToken!!, products)
                     .flatMap {
                         service.getVisualizaFCI(bearerToken)
-                        // logout
                     }
             }
-
+            .map { responseBody ->
+                ResponseEntity.ok(responseBody)
+            }
+            .onErrorResume { ex ->
+                // Retorna uma resposta de erro com status adequado
+                Mono.just(ResponseEntity.status(HttpStatus.BAD_REQUEST).body("Erro: ${ex.message}"))
+            }
     }
 }
